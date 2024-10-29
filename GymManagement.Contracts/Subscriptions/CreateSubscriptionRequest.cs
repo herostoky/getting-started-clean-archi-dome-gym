71115f964c09d9975e8f5fd9977c9e318626a7eb
Toky <herostoky@gmail.com>
2024-10-29 15:27:47 +0300

    Fix CreateSubscription

--- GymManagement.Contracts/Subscriptions/CreateSubscriptionRequest.cs
+++ GymManagement.Contracts/Subscriptions/CreateSubscriptionRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace GymManagement.Contracts.Subscriptions;
 
-public record CreateSubscriptionRequest(SubscriptionType SubscriptionType, Guid AdminGuid)
+public record CreateSubscriptionRequest(SubscriptionType SubscriptionType, Guid AdminId)
 {
 
 }
