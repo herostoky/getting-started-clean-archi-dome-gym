d24a505b5279574e65e0c3d462f82855d68ff648
Toky <herostoky@gmail.com>
2024-10-29 15:57:43 +0300

    CreateSubscription Add services

--- GymManagement.Api/Controllers/SubscriptionsController.cs
+++ GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -1,15 +1,19 @@
-﻿using GymManagement.Contracts.Subscriptions;
+﻿using GymManagement.Application.Services;
+using GymManagement.Contracts.Subscriptions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagement.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class SubscriptionsController: ControllerBase
+public class SubscriptionsController(ISubscriptionsService subscriptionsService)
+    : ControllerBase
 {
     [HttpPost]
     public IActionResult CreateSubscription(CreateSubscriptionRequest request)
     {
-        return Ok(request);
+        var subscriptionId = subscriptionsService.CreateSubscription(request.SubscriptionType.ToString(), request.AdminId);
+        var response = new SubscriptionResponse(subscriptionId, request.SubscriptionType);
+        return Ok(response);
     }
 }
